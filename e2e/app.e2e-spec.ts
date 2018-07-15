d8f744adc1c2824d881daf8245782ba2bbfd7ed2
ShinDarth <borzifrancesco@gmail.com>
2018-07-15 20:04:55 +0200

    wip

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -19,7 +19,7 @@ describe('ngx-duration-picker', () => {
         expect(page.durationPicker.isPresent()).toBe(true);
       });
 
-      it('Toggle disable should correctly work', () => {
+      it('toggle Disable should correctly work', () => {
         page.toggleButton.click();
 
         expect(page.dpYearsArrowUp.getAttribute('class')).toContain('disabled');
@@ -46,7 +46,7 @@ describe('ngx-duration-picker', () => {
         expect(page.dpSecondsInput.getAttribute('disabled')).toBeTruthy();
       });
 
-      it('Toggle enable should correctly work', () => {
+      it('toggle Enable should correctly work', () => {
         page.toggleButton.click();
         page.toggleButton.click();
 
@@ -72,6 +72,144 @@ describe('ngx-duration-picker', () => {
         expect(page.dpHoursInput.getAttribute('disabled')).toBeFalsy();
         expect(page.dpMinutesInput.getAttribute('disabled')).toBeFalsy();
         expect(page.dpSecondsInput.getAttribute('disabled')).toBeFalsy();
+      });
+
+      it('pushing the "set to value" button should correctly work', () => {
+        page.setValueButton.click();
+
+        expect(page.outputContainer.getText()).toBe('P7Y3M2W4DT1H3M5S');
+        expect(page.dpYearsInput.getAttribute('value')).toBe('7');
+        expect(page.dpMonthsInput.getAttribute('value')).toBe('3');
+        expect(page.dpWeeksInput.getAttribute('value')).toBe('2');
+        expect(page.dpDaysInput.getAttribute('value')).toBe('4');
+        expect(page.dpHoursInput.getAttribute('value')).toBe('1');
+        expect(page.dpMinutesInput.getAttribute('value')).toBe('3');
+        expect(page.dpSecondsInput.getAttribute('value')).toBe('5');
+      });
+
+      it('manually setting the duration value using inputs should correctly work', () => {
+        page.dpYearsInput.clear();
+        page.dpYearsInput.sendKeys('1');
+        expect(page.outputContainer.getText()).toBe('P1Y');
+        expect(page.dpValueContainer.getText()).toBe('P1Y');
+
+        page.dpMonthsInput.clear();
+        page.dpMonthsInput.sendKeys('2');
+        expect(page.outputContainer.getText()).toBe('P1Y2M');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M');
+
+        page.dpWeeksInput.clear();
+        page.dpWeeksInput.sendKeys('3');
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W');
+
+        page.dpDaysInput.clear();
+        page.dpDaysInput.sendKeys('4');
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4D');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4D');
+
+        page.dpHoursInput.clear();
+        page.dpHoursInput.sendKeys('5');
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H');
+
+        page.dpMinutesInput.clear();
+        page.dpMinutesInput.sendKeys('6');
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H6M');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H6M');
+
+        page.dpSecondsInput.clear();
+        page.dpSecondsInput.sendKeys('7');
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H6M7S');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H6M7S');
+
+        page.dpYearsInput.clear();
+        page.dpMonthsInput.clear();
+        page.dpWeeksInput.clear();
+        page.dpDaysInput.clear();
+        page.dpHoursInput.clear();
+        page.dpMinutesInput.clear();
+        page.dpSecondsInput.clear();
+        expect(page.outputContainer.getText()).toBe('PT0S');
+        expect(page.dpValueContainer.getText()).toBe('PT0S');
+      });
+
+      it('manually setting the duration value using arrows should correctly work', () => {
+        page.dpYearsArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y');
+        expect(page.dpValueContainer.getText()).toBe('P1Y');
+
+        page.dpYearsArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P2Y');
+        expect(page.dpValueContainer.getText()).toBe('P2Y');
+
+        page.dpYearsArrowDown.click();
+        expect(page.outputContainer.getText()).toBe('P1Y');
+        expect(page.dpValueContainer.getText()).toBe('P1Y');
+
+        page.dpMonthsArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y1M');
+        expect(page.dpValueContainer.getText()).toBe('P1Y1M');
+
+        page.dpYearsArrowDown.click();
+        expect(page.outputContainer.getText()).toBe('P1M');
+        expect(page.dpValueContainer.getText()).toBe('P1M');
+
+        page.dpYearsArrowDown.click();
+        expect(page.outputContainer.getText()).toBe('P1M');
+        expect(page.dpValueContainer.getText()).toBe('P1M');
+
+        page.dpYearsArrowUp.click();
+        page.dpYearsArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P2Y1M');
+        expect(page.dpValueContainer.getText()).toBe('P2Y1M');
+
+        page.dpYearsArrowDown.click();
+        page.dpMonthsArrowUp.click();
+        page.dpWeeksArrowUp.click();
+        page.dpWeeksArrowUp.click();
+        page.dpWeeksArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W');
+
+        page.dpWeeksArrowDown.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M2W');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M2W');
+
+        page.dpWeeksArrowUp.click();
+        page.dpDaysArrowUp.click();
+        page.dpDaysArrowUp.click();
+        page.dpDaysArrowUp.click();
+        page.dpDaysArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4D');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4D');
+
+        page.dpHoursArrowUp.click();
+        page.dpHoursArrowUp.click();
+        page.dpHoursArrowUp.click();
+        page.dpHoursArrowUp.click();
+        page.dpHoursArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H');
+
+        page.dpMinutesArrowUp.click();
+        page.dpMinutesArrowUp.click();
+        page.dpMinutesArrowUp.click();
+        page.dpMinutesArrowUp.click();
+        page.dpMinutesArrowUp.click();
+        page.dpMinutesArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H6M');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H6M');
+
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        page.dpSecondsArrowUp.click();
+        expect(page.outputContainer.getText()).toBe('P1Y2M3W4DT5H6M7S');
+        expect(page.dpValueContainer.getText()).toBe('P1Y2M3W4DT5H6M7S');
       });
     });
 
