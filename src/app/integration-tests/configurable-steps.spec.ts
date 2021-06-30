98d997e278244db060661344728167312cd30532
FrancescoBorzi <borzifrancesco@gmail.com>
2021-06-30 15:14:03 +0200

    chore: improvement

--- src/app/integration-tests/configurable-steps.spec.ts
+++ src/app/integration-tests/configurable-steps.spec.ts
@@ -24,59 +24,80 @@ describe('Configurable Steps (specific checks)', () => {
     return { page };
   };
 
-  it('should use initial steps', () => {
+  it('should use initial steps', async () => {
     const { page } = setup();
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual('0');
     page.clickElement(page.dpYearsArrowUp);
     page.clickElement(page.dpYearsArrowUp);
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_YEARS_STEPS * 2}`);
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual(`${INITIAL_YEARS_STEPS * 2}`);
     page.clickElement(page.dpYearsArrowDown);
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_YEARS_STEPS}`);
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual(`${INITIAL_YEARS_STEPS}`);
 
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('0');
     page.clickElement(page.dpMonthsArrowUp);
     page.clickElement(page.dpMonthsArrowUp);
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('2');
     page.clickElement(page.dpMonthsArrowDown);
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('1');
 
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual('0');
     page.clickElement(page.dpWeeksArrowUp);
     page.clickElement(page.dpWeeksArrowUp);
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_WEEKS_STEPS * 2}`);
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual(`${INITIAL_WEEKS_STEPS * 2}`);
     page.clickElement(page.dpWeeksArrowDown);
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_WEEKS_STEPS}`);
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual(`${INITIAL_WEEKS_STEPS}`);
 
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('0');
     page.clickElement(page.dpDaysArrowUp);
     page.clickElement(page.dpDaysArrowUp);
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('2');
     page.clickElement(page.dpDaysArrowDown);
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('1');
 
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual('0');
     page.clickElement(page.dpHoursArrowUp);
     page.clickElement(page.dpHoursArrowUp);
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_HOURS_STEPS * 2}`);
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual(`${INITIAL_HOURS_STEPS * 2}`);
     page.clickElement(page.dpHoursArrowDown);
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual(`${INITIAL_HOURS_STEPS}`);
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual(`${INITIAL_HOURS_STEPS}`);
 
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('0');
     page.clickElement(page.dpMinutesArrowUp);
     page.clickElement(page.dpMinutesArrowUp);
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('2');
     page.clickElement(page.dpMinutesArrowDown);
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('1');
 
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('0');
     page.clickElement(page.dpSecondsArrowUp);
     page.clickElement(page.dpSecondsArrowUp);
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('2');
     page.clickElement(page.dpSecondsArrowDown);
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('1');
   });
 
-  it('should use changed steps with replacement of incorrect values', () => {
+  it('should use changed steps with replacement of incorrect values', async () => {
     const { page } = setup();
     page.clear(page.dpYearsStepsInput);
     page.setInputValue(page.dpYearsStepsInput, `${UPDATED_YEARS_STEPS}`);
@@ -85,53 +106,74 @@ describe('Configurable Steps (specific checks)', () => {
     page.clear(page.dpHoursStepsInput);
     page.setInputValue(page.dpHoursStepsInput, (`${UPDATED_HOURS_STEPS}`));
 
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual('0');
     page.clickElement(page.dpYearsArrowUp);
     page.clickElement(page.dpYearsArrowUp);
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual('2');
     page.clickElement(page.dpYearsArrowDown);
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpYearsInput.value).toEqual('1');
 
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('0');
     page.clickElement(page.dpMonthsArrowUp);
     page.clickElement(page.dpMonthsArrowUp);
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('2');
     page.clickElement(page.dpMonthsArrowDown);
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpMonthsInput.value).toEqual('1');
 
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual('0');
     page.clickElement(page.dpWeeksArrowUp);
     page.clickElement(page.dpWeeksArrowUp);
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual(`${UPDATED_WEEKS_STEPS * 2}`);
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual(`${UPDATED_WEEKS_STEPS * 2}`);
     page.clickElement(page.dpWeeksArrowDown);
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toEqual(`${UPDATED_WEEKS_STEPS}`);
+    await page.whenStable();
+    expect(page.dpWeeksInput.value).toEqual(`${UPDATED_WEEKS_STEPS}`);
 
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('0');
     page.clickElement(page.dpDaysArrowUp);
     page.clickElement(page.dpDaysArrowUp);
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('2');
     page.clickElement(page.dpDaysArrowDown);
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpDaysInput.value).toEqual('1');
 
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual('0');
     page.clickElement(page.dpHoursArrowUp);
     page.clickElement(page.dpHoursArrowUp);
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual('2');
     page.clickElement(page.dpHoursArrowDown);
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpHoursInput.value).toEqual('1');
 
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('0');
     page.clickElement(page.dpMinutesArrowUp);
     page.clickElement(page.dpMinutesArrowUp);
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('2');
     page.clickElement(page.dpMinutesArrowDown);
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpMinutesInput.value).toEqual('1');
 
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('0');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('0');
     page.clickElement(page.dpSecondsArrowUp);
     page.clickElement(page.dpSecondsArrowUp);
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('2');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('2');
     page.clickElement(page.dpSecondsArrowDown);
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toEqual('1');
+    await page.whenStable();
+    expect(page.dpSecondsInput.value).toEqual('1');
   });
 });
--- src/app/integration-tests/negative-duration.spec.ts
+++ src/app/integration-tests/negative-duration.spec.ts
@@ -27,22 +27,23 @@ describe('Negative Duration (specific checks)', () => {
     expect(page.dpSignButtonIcon.classList.toString()).not.toContain('bs-icon-minus');
   });
 
-  it('pushing the "set to value" button should correctly work', () => {
+  it('pushing the "set to value" button should correctly work', async () => {
     const { page } = setup();
 
     page.clickElement(page.setValueButton);
+    await page.whenStable();
 
     expect(page.dpSignButtonIcon.classList.toString()).toContain('bs-icon-minus');
     expect(page.dpSignButtonIcon.classList.toString()).not.toContain('bs-icon-plus');
     expect(page.outputContainer.innerText).toBe('-P7Y3M2W4DT1H3M5S');
     expect(page.dpValueContainer.innerText).toBe('-P7Y3M2W4DT1H3M5S');
-    expect(page.dpYearsInput.getAttribute('ng-reflect-model')).toBe('7');
-    expect(page.dpMonthsInput.getAttribute('ng-reflect-model')).toBe('3');
-    expect(page.dpWeeksInput.getAttribute('ng-reflect-model')).toBe('2');
-    expect(page.dpDaysInput.getAttribute('ng-reflect-model')).toBe('4');
-    expect(page.dpHoursInput.getAttribute('ng-reflect-model')).toBe('1');
-    expect(page.dpMinutesInput.getAttribute('ng-reflect-model')).toBe('3');
-    expect(page.dpSecondsInput.getAttribute('ng-reflect-model')).toBe('5');
+    expect(page.dpYearsInput.value).toBe('7');
+    expect(page.dpMonthsInput.value).toBe('3');
+    expect(page.dpWeeksInput.value).toBe('2');
+    expect(page.dpDaysInput.value).toBe('4');
+    expect(page.dpHoursInput.value).toBe('1');
+    expect(page.dpMinutesInput.value).toBe('3');
+    expect(page.dpSecondsInput.value).toBe('5');
   });
 
   it('pushing the sign button should correctly change the sign', () => {
