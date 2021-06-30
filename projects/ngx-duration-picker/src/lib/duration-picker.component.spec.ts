828f5e6bdf20ba51dac143bb74ce781e2d6be198
FrancescoBorzi <borzifrancesco@gmail.com>
2021-06-30 12:38:02 +0200

    chore: Angular 11

--- projects/ngx-duration-picker/src/lib/duration-picker.component.spec.ts
+++ projects/ngx-duration-picker/src/lib/duration-picker.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { DurationPickerComponent } from './duration-picker.component';
@@ -9,7 +9,7 @@ describe('DurationPickerComponent', () => {
 
   const myFunc = () => 'something';
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DurationPickerComponent ],
       imports: [ FormsModule ],
