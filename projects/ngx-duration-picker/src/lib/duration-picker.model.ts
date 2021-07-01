873a40c4faff82a82583a8e7098b44a4f804f356
R Sepp <rob4@hv.ee>
2021-07-01 18:05:04 +0300

    feat: allow partial config (not all values should be mandatory) (#382)

--- projects/ngx-duration-picker/src/lib/duration-picker.model.ts
+++ projects/ngx-duration-picker/src/lib/duration-picker.model.ts
@@ -1,19 +1,19 @@
 export interface DurationPickerOptions {
-  showNegative: boolean;
-  showButtons: boolean;
-  showPreview: boolean;
-  showLetters: boolean;
-  showYears: boolean;
-  showMonths: boolean;
-  showWeeks: boolean;
-  showDays: boolean;
-  showHours: boolean;
-  showMinutes: boolean;
-  showSeconds: boolean;
-  zeroValue: string | null;
-  previewFormat: string;
-  customOutputFormat: string;
-  labels: {
+  showNegative?: boolean;
+  showButtons?: boolean;
+  showPreview?: boolean;
+  showLetters?: boolean;
+  showYears?: boolean;
+  showMonths?: boolean;
+  showWeeks?: boolean;
+  showDays?: boolean;
+  showHours?: boolean;
+  showMinutes?: boolean;
+  showSeconds?: boolean;
+  zeroValue?: string | null;
+  previewFormat?: string;
+  customOutputFormat?: string;
+  labels?: {
     years?: string;
     months?: string;
     weeks?: string;
@@ -22,7 +22,7 @@ export interface DurationPickerOptions {
     minutes?: string;
     seconds?: string;
   };
-  unitSteps: {
+  unitSteps?: {
     years?: number;
     months?: number;
     weeks?: number;
